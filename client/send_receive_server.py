ecb835ef8f45bfa0390c1e74c863cad85585304d
Rafael Bellotti <bellotti.rafael@gmail.com>
2016-10-26 01:00:33 +0000

    changes to tests

--- client/send_receive_server.py
+++ client/send_receive_server.py
@@ -1,5 +1,4 @@
-from socket import error as socket_error
-import socket
+from socket import *
 
 host = ''
 port = 5005
@@ -12,23 +11,20 @@ print ("Waiting to receive messages...")
 conn,addr = sock.accept()
 print ('Connection address:',addr)
 while True:
-      try:
-        print >>sys.stderr, 'connection from', client_address
-
-        # Receive the data in small chunks and retransmit it
-        while True:
-            data = connection.recv(16)
-            print >>sys.stderr, 'received "%s"' % data
-            if data:
-                print >>sys.stderr, 'sending data back to the client'
-                connection.send(data.encode('utf-8'))
-            else:
-                print >>sys.stderr, 'no more data from', client_address
-                break
-            
-    finally:
-        # Clean up the connection
-        connection.close()
-    
+	try:
+		print >>sys.stderr, 'connection from', client_address
+		# Receive the data in small chunks and retransmit it
+		while True:
+			data = connection.recv(16)
+			print >>sys.stderr, 'received "%s"' % data
+			if data:
+				print >>sys.stderr, 'sending data back to the client'
+				connection.send(data.encode('utf-8'))	
+			else:
+				print >>sys.stderr, 'no more data from', client_address
+				connection.close()
+				break
+      	except:
+		print ("Error!")      
 sock.close()
 os._exit(0)
